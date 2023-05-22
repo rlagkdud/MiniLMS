7d205b9f09125e08f24f3acd500493b632134a5e
rlagkdud <ha0936861@gmail.com>
2023-05-22 16:01:44 +0900

    register

--- src/main/java/com/zerobase/fastlms/main/controller/MainController.java
+++ src/main/java/com/zerobase/fastlms/main/controller/MainController.java
@@ -22,17 +22,13 @@ public class MainController {
     public String index(HttpServletRequest request) {
         
         /*
-        String email = "satcop@naver.com";
+        String email = "mailp@naver.com";
         String subject = " 안녕하세요. 제로베이스 입니다. ";
         String text = "<p>안녕하세요.</p><p>반갑습니다.</p>";
         
         mailComponents.sendMail(email, subject, text);
         */
-        /*String email = "ha0936861@gmail.com";
-        String subject = " 안녕하세요. 제로베이스 입니다. ";
-        String text = "<p>안녕하세요.</p><p>반갑습니다.</p>";
-
-        mailComponents.sendMail(email, subject, text);*/
+        
         System.out.println(request.getUserPrincipal().getName());
         
         return "index";
